c315313d40bf99e79969a2a6f344a68cc72533b8
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-05 13:09:10 +0200

    problem solved

---
+++ java-biginteger/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        BigInteger bi = new BigInteger(a);
+
+        System.out.println(bi.add(new BigInteger(b)));
+        System.out.println(bi.multiply(new BigInteger(b)));
+    }
+}
